8dd7db3be1a655fe5d5a92ec80267ab88ab1da34
gilzoide <gilzoide@gmail.com>
2026-02-28 17:14:20 -0300

    Spawna uma cerca logo no começo da partida

--- fence_spawn.gd
+++ fence_spawn.gd
@@ -6,7 +6,16 @@ const fences = [
 	preload("res://resources/fence/fence_medium.tscn"),
 ]
 
+
+func _ready():
+	call_deferred("spawn_fence")
+
+
 func _on_timer_timeout() -> void:
+	spawn_fence()
+
+
+func spawn_fence() -> void:
 	path.progress_ratio = randf()
 	var instance: Node2D = fences.pick_random().instantiate()
 	get_parent().add_child(instance)
