153aabdc4704a0010d7b36790b25addf74beb4b5
Paulo Bardes <bardes0022@gmail.com>
2026-02-28 13:15:16 -0300

    Agora com pulos!

--- resources/horse/horse.gd
+++ resources/horse/horse.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@onready var animation = $AnimationPlayer
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and not animation.is_playing():
 		position.y = event.position.y
+	elif event is InputEventMouseButton and event.pressed:
+		animation.play("jump")
+	
