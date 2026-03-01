42ee87d74f8820247d71fdb422b68614fa9413f5
Paulo Bardes <bardes0022@gmail.com>
2026-03-01 10:41:33 -0300

    Segue o mouse com delay e princípio de HUD

--- resources/horse/horse.gd
+++ resources/horse/horse.gd
@@ -2,15 +2,20 @@ extends Node2D
 
 @onready var animation = $AnimationPlayer
 
+@export var speed = 1.5
+
 var is_jumping: bool:
 	get():
 		return animation.is_playing()
 
+func _process(delta: float) -> void:
+	if not is_jumping:
+		var mouse_ypos = get_global_mouse_position().y
+		var delta_pos = mouse_ypos - position.y
+		position.y += speed * delta_pos * delta
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and not is_jumping:
-		position.y = event.position.y
-	elif event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed:
 		animation.play("jump")
 
 
